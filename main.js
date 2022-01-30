712e1c532d9b147100cde911d393892e45555b18
danielaTapiero <tapiieroangii@gmail.com>
2022-01-30 16:01:23 -0500

    se sube la carpeta con sus elementos

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector(".button").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+    });
+
+ScrollReveal().reveal('.imagen_principal');
+ScrollReveal().reveal('.new-card', { deley: 500 });
+ScrollReveal().reveal('.sect', { deley: 500 });
+ScrollReveal().reveal('.section', { deley: 500 });
+ScrollReveal().reveal('.social', { deley: 500 });
+ScrollReveal().reveal('.foo', { deley: 500 });
